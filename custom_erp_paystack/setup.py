67571a5a2ae50f5d1e7ec5b6c9985a900353b124
nkkb-io <e.kyei2406@gmail.com>
2026-06-21 22:24:31 +0000

    Update setup.py

--- custom_erp_paystack/setup.py
+++ custom_erp_paystack/setup.py
@@ -1,16 +1,1 @@
-from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
-setup(
-    name="custom_erp_paystack",
-    version="0.0.1",
-    description="Paystack payment gateway integration for ERPNext POS",
-    author="Eusol Organics",
-    author_email="eusolghana@gmail.com",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=install_requires
-)
